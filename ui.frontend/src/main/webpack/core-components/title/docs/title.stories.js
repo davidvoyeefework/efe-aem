f9ad7fdefeb97b8db5110f1ffc61483390509cc4
David Voye <dvoye@edelmanfinancialengines.com>
2023-02-02 14:50:13 -0700

    Heading & Subheads

--- ui.frontend/src/main/webpack/core-components/title/docs/title.stories.js
+++ ui.frontend/src/main/webpack/core-components/title/docs/title.stories.js
@@ -2,21 +2,21 @@ import TitleEFE from "../title.hbs";
 import "../../../site/main.scss";
 
 export default {
-  title: "Core Components/Title",
+  title: "Core Components/Headings",
   // More on argTypes: https://storybook.js.org/docs/html/api/argtypes
   argTypes: {
     text: {
       control: { type: "text" },
     },
     variant: {
-      options: ["heading-01", "heading-02", "heading-03"],
+      options: ["heading-01", "heading-02", "heading-03", "heading-04", "subhead-01", "subhead-02"],
       control: { type: "radio" },
     }
   },
 };
 
-const TemplateEFETitle = ({ label, ...args }) => TitleEFE({ ...args });
-export const TitleEFEDefault = TemplateEFETitle.bind();
+const TemplateHeadings = ({ label, ...args }) => TitleEFE({ ...args });
+export const TitleEFEDefault = TemplateHeadings.bind();
 TitleEFEDefault.args = {
   variant: "heading-01",
 }
