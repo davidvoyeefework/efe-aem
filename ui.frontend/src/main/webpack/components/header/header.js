aaac22e469f4c98f000b06b8478a0d21c6d8238c
David Voye <dvoye@edelmanfinancialengines.com>
2023-02-20 18:50:01 -0700

    Basic Nav Shell Ready For Review

--- ui.frontend/src/main/webpack/components/header/header.js
+++ ui.frontend/src/main/webpack/components/header/header.js
@@ -9,8 +9,11 @@ export default class Header {
     this.el = el;
 
     clickFunction("#nav-search-icon a","efe-nav-search");
+    clickFunction("#nav-search-iconT a","efe-nav-searchT");
     clickFunction("#search-close", "efe-nav-search");
+    clickFunction("#search-closeT", "efe-nav-searchT");
     clickFunction("#nav-hamburger", "nav-list-cta-group");
+    clickNav(".cmp-navigation__item--level-0 ");
     // $('body').style('background,' )
   }
 
@@ -36,4 +39,33 @@ const clickFunction = (listenItem, elmToShow) => {
   document.querySelector(listenItem).addEventListener("click", function() {
     showElement(elmToShow);
   });
+}
+
+const clickNav = (listenItem) => {
+  let NavItems = document.querySelectorAll(listenItem);
+  
+  NavItems.forEach(element => 
+      element.addEventListener("click", function() {
+
+        NavItems.forEach( el => {
+          if(el.classList.contains("cmp-navigation__item--active")) {
+            el.classList.remove("cmp-navigation__item--active");
+            let dropDownNav = el.children[2];
+            dropDownNav.style.display = 'none';
+          }
+        }
+        );
+   
+        element.classList.add("cmp-navigation__item--active");
+        let dropDownNav = element.children[2];
+        let displayStyle = getComputedStyle(dropDownNav, "display", null ).getPropertyValue("display");
+        if (displayStyle == "none") {
+          dropDownNav.style.display = 'block';
+        }
+        else if (displayStyle == "block") {
+          dropDownNav.style.display = 'none';
+        }
+
+      })
+    );
 }
