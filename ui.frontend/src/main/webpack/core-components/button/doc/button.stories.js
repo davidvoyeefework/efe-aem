3fe9232f816924643d43425f0b1f7985d474517a
David Voye <dvoye@edelmanfinancialengines.com>
2023-02-22 14:10:42 -0700

    work more

--- ui.frontend/src/main/webpack/core-components/button/doc/button.stories.js
+++ ui.frontend/src/main/webpack/core-components/button/doc/button.stories.js
@@ -10,7 +10,7 @@ export default {
       control: { type: "text" },
     },
     variant: {
-      options: ["cta-primary"],
+      options: ["cta-primary", "cta-secondary"],
       control: { type: "radio" },
     },
     link: {
