2ecf841b79674b82aa451c01b25c7c067f8f2ff3
Thomas Sebastian <T205242F@techdata.com>
2023-03-14 10:46:14 +0530

    border for container

--- ui.frontend/src/main/webpack/core-components/container/docs/container.stories.js
+++ ui.frontend/src/main/webpack/core-components/container/docs/container.stories.js
@@ -1,39 +1,43 @@
 import Handlebars from "handlebars/runtime.js";
-import Container from '../bg-color.hbs';
+import Container from "../bg-color.hbs";
 Handlebars.registerPartial("Container", Container);
 export default {
-  title: "Core Components/Container",
-  // More on argTypes: https://storybook.js.org/docs/html/api/argtypes
-  argTypes: {
-    text: {
-      control: { type: "text" },
+    title: "Core Components/Container",
+    // More on argTypes: https://storybook.js.org/docs/html/api/argtypes
+    argTypes: {
+        text: {
+            control: { type: "text" },
+        },
+        variant: {
+            options: ["bg-efe-gray-light", "bg-efe-gray-mid", "unset", "white"],
+            control: { type: "radio" },
+        },
+        maxWidth: {
+            options: ["1920", "1400", "none"],
+            control: { type: "radio" },
+        },
+        id: {
+            control: { type: "text" },
+        },
+        padding: {
+            options: ["unset", "26"],
+            control: { type: "radio" },
+        },
+        borderRadius: {
+            options: ["top-left", "top-right", "bottom-left", "bottom-right"],
+            control: { type: "select" },
+        },
+        border: {
+            control: 'boolean',
+        }
     },
-    variant: {
-      options: ["bg-efe-gray-light", "bg-efe-gray-mid", "unset", "white"],
-      control: { type: "radio" },
-    },
-    maxWidth: {
-      options: ["1920", "1400", "none"],
-      control: { type: "radio" },
-    },
-    id: {
-      control: { type: "text" },
-    },
-    padding: {
-      options: ["unset", "26"],
-      control: { type: "radio" },
-    },
-    borderRadius:{
-    options: ['top-left', 'top-right','bottom-left', 'bottom-right'],
-    control: { type: "select" },
-  },
-  },
 };
 
 const TemplateContainer = ({ label, ...args }) => Container({ ...args });
 export const BackgroundColor = TemplateContainer.bind();
 BackgroundColor.args = {
-  variant: "bg-efe-gray-light",
-  maxWidth: "unset",
-  padding: "unset"
-}
+    variant: "bg-efe-gray-light",
+    maxWidth: "unset",
+    padding: "unset",
+    border: false,
+};
