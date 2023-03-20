7ac3debd932238d805730deb149b5962885be93b
Leo Liu <shuchenl@adobe.com>
2023-03-19 18:13:24 -0700

    add meta count for leaf node

--- ui.apps/src/main/content/jcr_root/apps/efe/components/breadcrumb/breadcrumb.js
+++ ui.apps/src/main/content/jcr_root/apps/efe/components/breadcrumb/breadcrumb.js
@@ -4,7 +4,11 @@ use(function () {
 
     var index = properties.selectorIndex;
 
+    //var count = navItemList != null?navItemList.count:0;
+
     leafItem.selector = request.requestPathInfo.selectors[index];
+
+    leafItem.count = this.count != null?this.count+1:'';
 
     return leafItem;
 
