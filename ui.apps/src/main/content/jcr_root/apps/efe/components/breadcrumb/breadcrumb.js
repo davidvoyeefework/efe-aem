325f374db89d0d00b98dcd06ab0db5e4e4ed6f0f
Leo Liu <shuchenl@adobe.com>
2023-03-19 17:51:45 -0700

    add selector index propoerty

--- ui.apps/src/main/content/jcr_root/apps/efe/components/breadcrumb/breadcrumb.js
+++ ui.apps/src/main/content/jcr_root/apps/efe/components/breadcrumb/breadcrumb.js
@@ -2,6 +2,10 @@ use(function () {
 
     var leafItem = {};
 
+    var index = properties.selectorIndex;
+
+    leafItem.selector = request.requestPathInfo.selectors[index];
+
     return leafItem;
 
 });
