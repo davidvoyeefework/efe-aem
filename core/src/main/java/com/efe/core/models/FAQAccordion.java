cd1dfaf9c72584efc18f7158347394eb503563c9
Leo Liu <shuchenl@adobe.com>
2023-04-05 12:02:05 -0700

    refactor FAQ Accordion model

---
+++ core/src/main/java/com/efe/core/models/FAQAccordion.java
@@ -0,0 +1,23 @@
+package com.efe.core.models;
+
+import java.util.List;
+
+import com.efe.core.models.multifield.FAQ;
+
+public interface FAQAccordion {
+
+	/**
+	 * Gets the id.
+	 *
+	 * @return the id
+	 */
+	String getId();
+
+	/**
+	 * Gets the FAQ list.
+	 *
+	 * @return the FAQ list
+	 */
+	List<FAQ> getFaqList();
+
+}
