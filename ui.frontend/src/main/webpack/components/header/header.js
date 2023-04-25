66d697a96d652d1effe48701076bb7a76348f44b
Thomas Sebastian <T205242F@techdata.com>
2023-04-25 18:05:12 +0530

    change js and css to match aem updates to div

--- ui.frontend/src/main/webpack/components/header/header.js
+++ ui.frontend/src/main/webpack/components/header/header.js
@@ -58,6 +58,7 @@ const clickNav = (listenItem) => {
        
           NavItems.forEach((element) => {
             element.classList.remove("act");
+              // debugger;
             let dropDownNav = element.children[2];
             dropDownNav.style.display = "none";
           })
