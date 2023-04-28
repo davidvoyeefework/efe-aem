5ce638f3ff74ee4a75f6610004e8b7b18a73c26d
Vijaykanth Dondapati <vdondapati@adobe.com>
2023-04-28 13:47:57 +0530

    promotional modal changes

--- ui.frontend/src/main/webpack/components/modal-promotional/modal-promotional.js
+++ ui.frontend/src/main/webpack/components/modal-promotional/modal-promotional.js
@@ -1,10 +1,6 @@
 import A11yDialog from 'a11y-dialog'
-import { trapFocus } from "../../site/js/helper";
-var dialogEl = document.getElementById('modal-promotional')
-var dialog = new A11yDialog(dialogEl)
-dialog.on('show', function (dialogEl, event) {
-});
-
+var dialogEl = document.querySelector('.modal-promotional');
+var dialog = new A11yDialog(dialogEl);
 class ModalPromotional {
     constructor(el) {
       this.el = el;
@@ -20,29 +16,13 @@ class ModalPromotional {
     }
     showModal() {
         let splitValue = document.cookie.split(`; pop_up_displayed=`);
-        console.log(this.getCookie('pop_up_displayed'));
         if(splitValue.length < 2 || !splitValue[1].startsWith('true')) {
             dialog.show();
             let pop_disp = new Date();
-            pop_disp = pop_disp.setHours(today.getHours()+4);
+            pop_disp.setTime(pop_disp.getTime() + (4 * 60 * 60 * 1000));
             let expires = 'expires=' + pop_disp.toUTCString();
             document.cookie = 'pop_up_displayed=true;' + expires + ';path=/';
         }
     }
-    getCookie(cname) {
-        let name = cname + "=";
-        let decodedCookie = decodeURIComponent(document.cookie);
-        let ca = decodedCookie.split(';');
-        for(let i = 0; i <ca.length; i++) {
-          let c = ca[i];
-          while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-          }
-          if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-          }
-        }
-        return "";
-      }
 }
 export default ModalPromotional;
