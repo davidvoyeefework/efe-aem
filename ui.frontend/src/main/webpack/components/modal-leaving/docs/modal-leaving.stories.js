c5651b6c5999cd313ce00a90d8404df509611788
vijaykanth  d <vdondapati@adobe.com>
2023-04-28 14:28:43 +0000

    Bugfix/modal leaving issue

--- ui.frontend/src/main/webpack/components/modal-leaving/docs/modal-leaving.stories.js
+++ ui.frontend/src/main/webpack/components/modal-leaving/docs/modal-leaving.stories.js
@@ -1,7 +1,7 @@
 import ModalLeaving from "../modal-leaving.hbs";
 
 export default {
-  title: "Components/modal leaving",
+  title: "Components/modal/modal leaving",
   // More on argTypes: https://storybook.js.org/docs/html/api/argtypes
   argTypes: {},
 };
---
+++ ui.frontend/src/main/webpack/components/modal-promotional/modal-promotional.js
@@ -0,0 +1,28 @@
+import A11yDialog from 'a11y-dialog'
+var dialogEl = document.querySelector('.modal-promotional');
+var dialog = new A11yDialog(dialogEl);
+class ModalPromotional {
+    constructor(el) {
+      this.el = el;
+      this.intializeModal();
+    }
+    static init(el) {
+        return new ModalPromotional(el);
+      }
+    intializeModal() {
+        setTimeout(()=>{
+            this.showModal();
+        }, 1000)
+    }
+    showModal() {
+        let splitValue = document.cookie.split(`; pop_up_displayed=`);
+        if(splitValue.length < 2 || !splitValue[1].startsWith('true')) {
+            dialog.show();
+            let pop_disp = new Date();
+            pop_disp.setTime(pop_disp.getTime() + (4 * 60 * 60 * 1000));
+            let expires = 'expires=' + pop_disp.toUTCString();
+            document.cookie = 'pop_up_displayed=true;' + expires + ';path=/';
+        }
+    }
+}
+export default ModalPromotional;
