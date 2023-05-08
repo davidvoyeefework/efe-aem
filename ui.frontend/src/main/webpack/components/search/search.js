e5338180dfdb14fd5e24bf2a85bea1e069a18a8a
Thomas Sebastian <sebastia@adobe.com>
2023-05-08 09:55:32 +0000

    Bugfix/eada 521 header bugfix

--- ui.frontend/src/main/webpack/components/search/search.js
+++ ui.frontend/src/main/webpack/components/search/search.js
@@ -1,34 +1,9 @@
 export default class Search {
   constructor(el) {
     this.el = el;
-    this.el
-      .querySelector(".cmp-search__cta")
-      .addEventListener("click", this.handleSearchButtonClick.bind(this));
   }
 
   static init(el) {
     return new Search(el);
-  }
-
-  handleSearchButtonClick(event) {
-    event.preventDefault();
-
-    const searchInputValue = this.el.querySelector(".cmp-search__input")?.value;
-    if (!searchInputValue) {
-      return;
-    }
-
-    /**
-     * Check if there's a match for the searched term, if it exists go there.
-     */
-
-    const results = document.querySelector(".cmp-search__results");
-    if (results) {
-      results.querySelectorAll(".cmp-search__item-title")?.forEach((result) => {
-        if (result.innerText() === searchInputValue) {
-          result.parentElement?.click();
-        }
-      });
-    }
   }
 }
