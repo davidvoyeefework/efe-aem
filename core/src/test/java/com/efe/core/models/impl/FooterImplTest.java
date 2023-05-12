39ebe56b6fed268e6dbd9399e0684bf48f760a6b
Kartik Karnayil <karnayil@adobe.com>
2023-05-12 08:52:12 +0530

    Added missing java doc

--- core/src/test/java/com/efe/core/models/impl/FooterImplTest.java
+++ core/src/test/java/com/efe/core/models/impl/FooterImplTest.java
@@ -3,7 +3,6 @@ package com.efe.core.models.impl;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.lenient;
-import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -51,6 +50,7 @@ class FooterImplTest {
 	@Mock
 	Externalizer externalizer;
 	
+	/** The efe service. */
 	@Mock
 	EfeService efeService;
 
