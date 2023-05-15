18523e74a9c59743eeae56324c5ca9bacc8e650b
Kartik Karnayil <karnayil@adobe.com>
2023-05-15 14:30:06 +0530

    Removed console log

--- ui.apps/src/main/content/jcr_root/apps/efe/components/form/text/clientlibs/site/js/text.js
+++ ui.apps/src/main/content/jcr_root/apps/efe/components/form/text/clientlibs/site/js/text.js
@@ -14,7 +14,6 @@
  * limitations under the License.
  ******************************************************************************/
 (function() {
-    console.log("apps");
     "use strict";
     var NS = "cmp";
     var IS = "efeText";
