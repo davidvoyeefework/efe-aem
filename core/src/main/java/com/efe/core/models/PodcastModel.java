2152acd6ebcbe5c5eea3cf90f48b325309a4a3ff
Kartik Karnayil <karnayil@adobe.com>
2023-05-31 09:20:02 +0000

    Feature/eada 496 podcast archivelist

---
+++ core/src/main/java/com/efe/core/models/PodcastModel.java
@@ -0,0 +1,44 @@
+package com.efe.core.models;
+
+import com.efe.core.models.bean.Podcast;
+
+/**
+ * The Interface PodcastArchiveList.
+ */
+public interface PodcastModel {
+
+	/**
+	 * Gets the id.
+	 *
+	 * @return the id
+	 */
+	String getId();
+
+	/**
+	 * Gets the podcast.
+	 *
+	 * @return the podcast
+	 */
+	Podcast getPodcast(); 
+	/**
+	 * Checks if is empty.
+	 *
+	 * @return the empty
+	 */
+	boolean isEmpty();
+
+	/**
+	 * Checks if is api error.
+	 *
+	 * @return the isApiError
+	 */
+	boolean isApiError();
+
+	/**
+	 * Gets the file reference.
+	 *
+	 * @return the fileReference
+	 */
+	String getFileReference();
+
+}
