5d543bfd5100bdb4d77421f15f3a50726f0460ca
Kartik Karnayil <karnayil@adobe.com>
2023-06-02 13:30:03 +0530

    Title final changes

--- core/src/main/java/com/efe/core/models/impl/EfeTitleImpl.java
+++ core/src/main/java/com/efe/core/models/impl/EfeTitleImpl.java
@@ -5,16 +5,13 @@ import java.util.Objects;
 import javax.annotation.PostConstruct;
 
 import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.Via;
-import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 import org.apache.sling.models.annotations.via.ResourceSuperType;
-import org.apache.sling.models.factory.ModelFactory;
 
 import com.adobe.cq.export.json.ExporterConstants;
 import com.adobe.cq.wcm.core.components.commons.link.Link;
@@ -22,11 +19,10 @@ import com.adobe.cq.wcm.core.components.models.Title;
 import com.adobe.cq.wcm.core.components.models.datalayer.ComponentData;
 import com.efe.core.constants.PlannerLocationConstants;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class EfeTitleImpl.
  */
-@Model(adaptables = { SlingHttpServletRequest.class, Resource.class }, adapters = Title.class, resourceType = {
+@Model(adaptables = { SlingHttpServletRequest.class }, adapters = Title.class, resourceType = {
 		EfeTitleImpl.RESOURCE_TYPE }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 @Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
 public class EfeTitleImpl implements Title {
@@ -40,16 +36,12 @@ public class EfeTitleImpl implements Title {
 	/** The Constant RESOURCE_TYPE. */
 	public static final String RESOURCE_TYPE = "efe/components/title";
 
-	/** The model factory. */
-	@OSGiService
-	private ModelFactory modelFactory;
-
 	/** The request. */
 	@Self
 	private SlingHttpServletRequest request;
 
 	/** The title. */
-	@Self // Indicates that we are resolving the current resource
+	@Self
 	@Via(type = ResourceSuperType.class)
 	private Title title;
 
@@ -61,20 +53,16 @@ public class EfeTitleImpl implements Title {
 	private String text;
 
 	/**
-	 * Inits the.
+	 * Inits the model.
 	 */
 	@PostConstruct
 	protected void init() {
-
 		if (!Objects.nonNull(title) || !Objects.nonNull(title.getText())) {
 			return;
 		}
-
 		text = title.getText();
-
 		if (includeLocationInTitle && null != request.getRequestPathInfo().getSelectors()) {
-
-			String[] selectors = request.getRequestPathInfo().getSelectors();
+			final String[] selectors = request.getRequestPathInfo().getSelectors();
 			if (selectors.length == 2) {
 				text = text.replace(SELECTOR_PLACEHOLDER_0, selectors[0]);
 				text = text.replace(SELECTOR_PLACEHOLDER_1, selectors[1]).replace(PlannerLocationConstants.HYPHEN,
@@ -140,7 +128,7 @@ public class EfeTitleImpl implements Title {
 	 */
 	@Override
 	public String getType() {
-		return title.getId();
+		return title.getType();
 	}
 
 	/**
