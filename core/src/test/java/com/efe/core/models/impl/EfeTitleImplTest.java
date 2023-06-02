0c4df8f0c6cc0b29bef0152f04145130d1e969ad
Kartik Karnayil <karnayil@adobe.com>
2023-06-02 11:57:22 +0530

    Title extension first draft

---
+++ core/src/test/java/com/efe/core/models/impl/EfeTitleImplTest.java
@@ -0,0 +1,14 @@
+package com.efe.core.models.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EfeTitleImplTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
