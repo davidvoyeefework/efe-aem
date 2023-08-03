50faf97bf188346c44cb06415081ef60057e310e
Rahul Kumar <rahulkumar83@deloitte.com>
2023-08-03 12:52:12 +0530

    added variation option for storybook

--- ui.frontend/src/main/webpack/fe-components/components/fe-form/docs/fe-form.stories.js
+++ ui.frontend/src/main/webpack/fe-components/components/fe-form/docs/fe-form.stories.js
@@ -4,15 +4,15 @@ import CallBackForm from "../fe-call-back-form.hbs";
 export default {
     title: "FE Components/form",
     argTypes: {
-        sponsor: {
-            options: ["Boeing", "ATT"],
+        variation: {
+            options: ["Default", "theme-vanguard" , "theme-aon", "theme-ssga"],
             control: { type: "radio" },
         },
     },
 };
 
-const TemplateSignup = ({ label, ...args }) => Form();
+const TemplateSignup = ({ label, ...args }) => Form({...args});
 export const SignUp = TemplateSignup.bind();
 
-const TemplateCallBack = ({ label, ...args }) => CallBackForm();
+const TemplateCallBack = ({ label, ...args }) => CallBackForm({...args});
 export const CallBack = TemplateCallBack.bind();
