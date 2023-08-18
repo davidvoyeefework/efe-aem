6b2ea680ae926b918e4d4bd096f8e029149ebf2b
Rahul Kumar <rahulkumar83@deloitte.com>
2023-08-18 16:10:05 +0530

    code for bright cove

---
+++ ui.frontend/src/main/webpack/fe-components/components/fe-brightcove/docs/fe-brightcove.stories.js
@@ -0,0 +1,15 @@
+import Brightcove from "../fe-brightcove.hbs";
+import Handlebars from "handlebars/runtime.js";
+Handlebars.registerPartial("Brightcove", Brightcove);
+export default {
+    title: "FE Components/Brightcove",
+    argTypes: {
+        variation: {
+            options: ["full-width", "large-height"],
+            control: { type: "radio" },
+        },
+    },
+};
+
+const TemplateBrightcove = ({ label, ...args }) => Brightcove({...args});
+export const BrightcoveComp = TemplateBrightcove.bind();
