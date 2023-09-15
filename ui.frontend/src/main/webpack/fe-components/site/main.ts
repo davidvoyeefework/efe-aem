2a72d5662989c5fd200d84ac7868720b3789a234
Vijaykanth Dondapati <vdondapati@adobe.com>
2023-09-15 19:36:19 +0530

    removed unused api calls

--- ui.frontend/src/main/webpack/fe-components/site/main.ts
+++ ui.frontend/src/main/webpack/fe-components/site/main.ts
@@ -1,11 +1,9 @@
 import FePage from "../components/fe-pages/fe-page";
 import FeHeader from "../components/fe-header/fe-header";
 import FeFooter from "../components/fe-footer/fe-footer";
-import FeReplaceDataWithPlaceholder from "../components/fe-replace-data-with-placeholder/fe-replace-data-with-placeholder";
 import FeMembershipDashboardButton from "../components/fe-membership-dashboard-button/fe-membership-dashboard-button";
 import "./main.scss";
 new FePage();
 new FeHeader();
 new FeFooter();
 new FeMembershipDashboardButton();
-new FeReplaceDataWithPlaceholder();
