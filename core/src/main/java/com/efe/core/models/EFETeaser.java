5c8e4f24da5e59b6f585fc3793cbf9f98bafe39d
David Voye <dvoye@edelmanfinancialengines.com>
2023-10-23 15:25:36 -0700

    Custom extension for teaser tags

---
+++ core/src/main/java/com/efe/core/models/EFETeaser.java
@@ -0,0 +1,21 @@
+package com.efe.core.models;
+
+import java.util.List;
+
+import com.adobe.cq.wcm.core.components.models.Teaser;
+import com.efe.core.bean.LinkBean;
+
+/**
+ * The Interface EFETeaser Model.
+ */
+
+public interface EFETeaser extends Teaser {
+
+	/**
+	 * Gets the tags.
+	 *
+	 * @return the tags
+	 */
+	List<LinkBean> getTags();
+
+}
