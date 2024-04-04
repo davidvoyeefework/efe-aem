9805b06a44c6ac549f9e61d46557bb56a021892e
David Voye <dvoye@edelmanfinancialengines.com>
2024-04-04 15:31:08 -0700

    Bi directional JS for tab overflow

--- ui.frontend/src/main/webpack/framework/initializer.js
+++ ui.frontend/src/main/webpack/framework/initializer.js
@@ -174,10 +174,103 @@ window.addEventListener("resize", tabsCornish);
 
 //GSAP Animation Platform for Cornish Tabs
 document.addEventListener("DOMContentLoaded", (event) => {
+
   const cornishTabEl = document.querySelector(".tabs-cornish .cmp-tabs__tablist");
+
   if (cornishTabEl != null) {
     gsap.registerPlugin(ScrollTrigger,ScrollToPlugin);
+    const scrollEl = document.getElementById("cornish-tab-nav");    
 
+    const imgTag = document.createElement("div");
+    const imgTagBack = document.createElement("div");
+    imgTag.classList.add("scrollIcon");
+    imgTagBack.classList.add("scrollIconBack");    
+    wrapper.appendChild(imgTag);
+    wrapper.appendChild(imgTagBack);
+    const scrollIcon = document.querySelector("#wrapper > .scrollIcon");
+    const scrollIconBack = document.querySelector("#wrapper > .scrollIconBack");
+
+    scrollIcon.addEventListener("click", () => {
+      let scrollWidth = scrollEl.scrollWidth;
+      let viewPortWidth = scrollEl.offsetWidth;
+      let overFlowWidth = scrollWidth - viewPortWidth -.5;
+      let tabOlElement = document.querySelectorAll("#cornish-tab-nav li");
+      let tabNumber = tabOlElement.length;
+      
+      let widthTally = 0;
+      for (let i = 0; i <= tabNumber; i++) {
+        let tabElWidth = tabOlElement[i].clientWidth + 56;
+
+        widthTally = widthTally + tabElWidth;
+
+        if (widthTally >= viewPortWidth) {
+          widthTally = widthTally - tabOlElement[i].clientWidth -56; 
+          scrollEl.scrollLeft += widthTally;      
+          return;
+        }
+      }
+    });
+
+    let calledTimes = 0;
+    scrollIconBack.addEventListener("click", () => {
+      let scrollWidth = scrollEl.scrollWidth;
+      let viewPortWidth = scrollEl.offsetWidth;
+      let overFlowWidth = scrollWidth - viewPortWidth -.5;
+      let tabOlElement = document.querySelectorAll("#cornish-tab-nav li");
+      let tabNumber = tabOlElement.length;
+      calledTimes ++;
+
+      if (calledTimes == 2) {
+        scrollEl.scrollLeft = 0;
+        return;
+      }
+      
+      let widthTally = 0;
+
+      for (let i = tabNumber -1; i >= 0; i--) {
+        let tabElWidth = tabOlElement[i].clientWidth + 56;
+        tabOlElement[i].clientWidth + 56;
+
+        widthTally = widthTally + tabElWidth;
+
+        if (widthTally >= viewPortWidth) {
+          let newTally = 0;
+          for (let x = 0; x <= tabNumber; x++) {
+            if (x == i) {
+              scrollEl.scrollLeft = newTally - 30;
+              return;
+            }
+            newTally += tabOlElement[x].clientWidth + 56;
+          }
+
+          widthTally = widthTally - tabOlElement[i].clientWidth  + 56;; 
+          let position = overFlowWidth - widthTally;
+          scrollEl.scrollLeft -= widthTally;      
+          return;
+        }
+      }
+    });
+
+    // SCROLL ICON BEHAVIOR
+    scrollEl.addEventListener("scroll", (event) => {
+      let scroll = scrollEl.scrollLeft;
+      let scrollWidth = scrollEl.scrollWidth;
+      let viewPortWidth = scrollEl.offsetWidth;
+      let scrollEnd = scrollWidth - viewPortWidth -.5;
+      const wrapper = document.getElementById("wrapper");
+
+      if (scroll >= scrollEnd ) {
+        wrapper.classList.add("no-icon");
+        wrapper.classList.add("reverse-icon");
+      }
+      if (scroll == 0) {
+        wrapper.classList.remove("no-icon");
+        wrapper.classList.remove("reverse-icon");
+      }
+
+    });
+
+    // STICKY SECTION ON SCROLL BEHAVIOR
     const tl = gsap.timeline({
       scrollTrigger: {
           trigger: "#trigger",
