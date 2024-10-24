7492b6d8aabb473691f6fd3e875f91d7da63153b
jrodriquez@edelmanfinancialengines.com <jrodriquez@edelmanfinancialengines.com>
2024-10-24 10:13:11 -0500

    Adding CDT scheduler component.

---
+++ core/src/main/java/com/efe/core/models/CDTScheduler.java
@@ -0,0 +1,15 @@
+package com.efe.core.models;
+
+/**
+ * The Interface Forms.
+ */
+public interface CDTScheduler {
+
+	/**
+	 * Gets the form url.
+	 *
+	 * @return the form url
+	 */
+	String getSFLeadId();
+
+}
