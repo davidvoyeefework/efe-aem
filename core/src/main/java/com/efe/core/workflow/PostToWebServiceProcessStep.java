6c5651be93a67914000b78e94ba21815d4638f63
jrodriquez@edelmanfinancialengines.com <jrodriquez@edelmanfinancialengines.com>
2024-11-22 11:52:01 -0600

    Attempting to add RS256 support.

--- core/src/main/java/com/efe/core/workflow/PostToWebServiceProcessStep.java
+++ core/src/main/java/com/efe/core/workflow/PostToWebServiceProcessStep.java
@@ -8,6 +8,8 @@ import java.util.Map;
 import java.util.Iterator;
 import java.io.IOException;
 import java.net.URLEncoder;
+import java.security.Security;
+import com.nimbusds.jose.crypto.bc.BouncyCastleProviderSingleton;
 import com.nimbusds.jose.*;
 import com.nimbusds.jose.crypto.*;
 import com.nimbusds.jwt.*;
@@ -307,7 +309,7 @@ public class PostToWebServiceProcessStep implements WorkflowProcess {
     }
     
     private String getJWTHeader() throws JOSEException {
-
+        Security.addProvider(BouncyCastleProviderSingleton.getInstance());
         // Create the JWT header
         JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.RS256).keyID(efeService.getPartnerAPIAuthKID()).build();
         // Create the JWT claims set (payload)
