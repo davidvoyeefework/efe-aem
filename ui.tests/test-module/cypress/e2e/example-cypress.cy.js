e5d39475372d7973632885cbc3421b021f2a81db
Pedro Segura <psegura@edelmanfinancialengines.com>
2025-02-10 15:33:23 -0500

    fail testing script on purpose

--- ui.tests/test-module/cypress/e2e/example-cypress.cy.js
+++ ui.tests/test-module/cypress/e2e/example-cypress.cy.js
@@ -1,5 +1,5 @@
 describe("template spec", () => {
     it("passed", () => {
-        cy.visit("https://example.cypress.io/")
+        cy.visit("https://example.cypress.io/test-page-not-found/")
     });
 });
