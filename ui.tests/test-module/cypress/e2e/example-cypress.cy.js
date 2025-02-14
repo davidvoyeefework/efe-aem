2e87fbeef674278e632911bde4324f84f8276860
Pedro Segura <psegura@edelmanfinancialengines.com>
2025-02-14 15:39:55 -0500

    Remove latest changes

--- ui.tests/test-module/cypress/e2e/example-cypress.cy.js
+++ ui.tests/test-module/cypress/e2e/example-cypress.cy.js
@@ -1,6 +1,6 @@
 describe("template spec", () => {
     it("passed", () => {
-        cy.task("log", Cypress.env());
+        // cy.task("log", Cypress.env());
         cy.visit("https://example.cypress.io/")
     });
 });
