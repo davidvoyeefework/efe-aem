a330dc434cbc2a0dbeea63414b3a983d5a44dbcd
jrodriquez@edelmanfinancialengines.com <jrodriquez@edelmanfinancialengines.com>
2025-02-28 10:43:20 -0600

    Change object check.

--- ui.frontend/src/main/webpack/fe-components/site/js/utility.js
+++ ui.frontend/src/main/webpack/fe-components/site/js/utility.js
@@ -3,7 +3,7 @@ import { captureScIdFromUrl } from "./helper";
 export const availableThroughText = (key) => {
   const windowDataObj = window.aemfe;
   let topFee = 0.0;
-  if (windowDataObj !== null && windowDataObj.sponsoredFees.length > 0) {
+  if (windowDataObj?.sponsoredFees?.feeScheduleTiers) {
     windowDataObj.sponsoredFees.feeScheduleTiers.forEach((elem) => {
       if (elem.feeRate > topFee) {
         topFee = elem.feeRate;
