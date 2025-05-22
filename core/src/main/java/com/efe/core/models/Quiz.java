6b5585ca1975dc96b7a29dc14a04b9f2b3299a97
David Voye <dvoye@edelmanfinancialengines.com>
2025-05-22 11:27:20 -0700

    Quiz checkin

--- core/src/main/java/com/efe/core/models/Quiz.java
+++ core/src/main/java/com/efe/core/models/Quiz.java
@@ -1,10 +1,14 @@
 package com.efe.core.models;
 
-import java.util.List;
-
-import com.efe.core.models.multifield.QuizAnswer;
-
 public interface Quiz {
+
+    // Intro Pane
+
+    public String getIntroHeadline();
+
+    public String getIntroSubheadline();
+
+    public String getIntroCta();    
 
     public String getQuizLength();
 
@@ -31,7 +35,75 @@ public interface Quiz {
 
     public String getResultsCta();
 
-    public List<QuizAnswer> getQuizAnswer();
+    // Answers
+
+        // 0 Selections
+        public String getIcon();
+
+        public String getAnswerCtaLink();
+
+        public String getAnswerCtaText();
+
+        public String getHeadline();
+
+        public String getBodyCopy();
+
+        // 1-2 Selections
+        public String getIcon12();
+
+        public String getAnswerCtaLink12();
+
+        public String getAnswerCtaText12();
+
+        public String getHeadline12();
+
+        public String getBodyCopy12();
+
+        // 3-4 Selections
+        public String getIcon34();
+
+        public String getAnswerCtaLink34();
+
+        public String getAnswerCtaText34();
+
+        public String getHeadline34();
+
+        public String getBodyCopy34();        
+
+        //  5 - 6 selections
+        public String getIcon56();
+
+        public String getAnswerCtaLink56();
+
+        public String getAnswerCtaText56();
+
+        public String getHeadline56();
+
+        public String getBodyCopy56();    
+        
+        //  7 - 8 selections
+        public String getIcon78();
+
+        public String getAnswerCtaLink78();
+
+        public String getAnswerCtaText78();
+
+        public String getHeadline78();
+
+        public String getBodyCopy78();        
+
+        // 9 - 10 selections
+        public String getIcon910();
+
+        public String getAnswerCtaLink910();
+
+        public String getAnswerCtaText910();
+
+        public String getHeadline910();
+
+        public String getBodyCopy910();         
+
+
 
     
 }
