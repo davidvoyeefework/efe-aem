3363d7bf7485932e768c020bccabaac520f930f4
David Voye <dvoye@edelmanfinancialengines.com>
2025-06-16 15:03:20 -0700

    Change to external Name vs City

--- core/src/main/java/com/efe/core/utils/OfficeLocationsUtil.java
+++ core/src/main/java/com/efe/core/utils/OfficeLocationsUtil.java
@@ -2,14 +2,10 @@ package com.efe.core.utils;
 
 import java.util.List;
 import java.util.Objects;
-import javax.jcr.Session;
 
 import javax.jcr.Node;
-import javax.jcr.Repository;
 import javax.jcr.RepositoryException;
-import javax.jcr.SimpleCredentials;
 
-import org.apache.jackrabbit.commons.JcrUtils;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.slf4j.Logger;
@@ -37,9 +33,7 @@ public class OfficeLocationsUtil {
 			ResourceResolver resourceResolver) {
 		try {
 			List<OfficesLocations> officesLocations = jsonObj.getOfficesLocations();
-
-			officesLocations.removeIf( obj -> obj.getName() == "Phoenix - Virtual Advisors");
-
+			officesLocations.removeIf( obj -> obj.getExternalName() == "National Advisor Center");
 			String officesLocationsRootPath = FolderUtil.createFolder(childPathPlanner,
 					PlannerLocationConstants.OFFICE_LOCATIONS,PlannerLocationConstants.OFFICE_LOCATIONS, resourceResolver);
 			int officesLocationsCount = 1;
@@ -69,25 +63,11 @@ public class OfficeLocationsUtil {
 				
 				Node plannerOfficesLocationsNode = plannerOfficesLocationsResource.adaptTo(Node.class);
 
-				// If master node inside array of OfficeLocations has Phoenix for Prop value delete
-				String propertyName = plannerOfficesLocationsNode.getProperty("city").toString();
-				// if (propertyName == "Phoenix") {
-				// 	Repository repository = JcrUtils.getRepository("http://localhost:4502/crx/server");
-				// 	javax.jcr.Session session = repository.login( new SimpleCredentials("admin", "admin".toCharArray()));
-				// 	plannerOfficesLocationsNode.remove();
-				// 	session.save();
-				// 	session.logout();
-				}
-
-
-
 				NodePropertyManagerUtil.setPropertyIfNonNull(plannerOfficesLocationsNode, PlannerLocationConstants.ID,
 						officesLocationsObj.getId());
 
-				if (officesLocationsObj.getCity() != "Phoenix") {	
-					NodePropertyManagerUtil.setPropertyIfNonNull(plannerOfficesLocationsNode, PlannerLocationConstants.CITY,
-					officesLocationsObj.getCity());					
-				}					
+				NodePropertyManagerUtil.setPropertyIfNonNull(plannerOfficesLocationsNode, PlannerLocationConstants.CITY,
+						officesLocationsObj.getCity());					
 
 				NodePropertyManagerUtil.setPropertyIfNonNull(plannerOfficesLocationsNode,
 						PlannerLocationConstants.STATE, officesLocationsObj.getState());
