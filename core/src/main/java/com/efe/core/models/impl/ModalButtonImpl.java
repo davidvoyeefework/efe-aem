3aa3d852e9d87a7c6944fec5f2c3bee7eb86ed8a
jrodriquez@edelmanfinancialengines.com <jrodriquez@edelmanfinancialengines.com>
2025-12-03 12:49:30 -0600

    button modal component functionality added.

--- core/src/main/java/com/efe/core/models/impl/ModalButtonImpl.java
+++ core/src/main/java/com/efe/core/models/impl/ModalButtonImpl.java
@@ -1,13 +1,87 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.efe.core.models.impl;
+
+import com.adobe.cq.export.json.ExporterConstants;
+import com.efe.core.models.ModalButton;
+import com.efe.core.services.DynamicMediaService;
+import com.efe.core.utils.EFEUtil;
+import com.efe.core.utils.LinkUtil;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 /**
  *
  * @author jrodriquez
  */
-public class ModalButtonImpl {
+@Model(adaptables = Resource.class, adapters = ModalButton.class, resourceType = {
+        ModalButtonImpl.RESOURCE_TYPE }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
+public class ModalButtonImpl implements ModalButton {
+        /** The Constant RESOURCE_TYPE. */
+    public static final String RESOURCE_TYPE = "efe/components/modalbutton";
     
+    /** The resource resolver. */
+    @SlingObject
+    private ResourceResolver resourceResolver;
+
+    /**
+     * The current resource.
+     */
+    @Self
+    private Resource resource;
+
+    /** The button text. */
+    @ValueMapValue
+    private String buttonText;
+
+    /** The button url. */
+    @ValueMapValue
+    private String embedFragment;
+
+    /** The id. */
+    @ValueMapValue
+    private String id;
+
+    /**
+     * Injecting dynamicMediaService
+     *
+     */
+    @OSGiService
+    private DynamicMediaService dynamicMediaService;
+
+    /**
+     * Gets the Button Text.
+     *
+     * @return the Button Text
+     */
+    @Override
+    public String getButtonText() {
+        return buttonText;
+    }
+
+    /**
+     * Gets the Button Url.
+     *
+     * @return the Button Url
+     */
+    @Override
+    public String getEmbedFragment() {
+        return LinkUtil.getFormattedLink(embedFragment, resourceResolver);
+    }
+
+    /**
+     * Gets the id.
+     *
+     * @return the id
+     */
+    @Override
+    public String getId() {
+        return EFEUtil.getId(resource);
+    }
 }
